2f4a166e20ec862f1b7180159098cf019ff35c9f
Xiang Han <80561550+magiciansz@users.noreply.github.com>
2024-03-11 18:08:48 +0800

    add etl pipeline and helper function file

---
+++ etl_helper.py
@@ -0,0 +1,98 @@
+import json
+import requests
+
+#this function takes in your OneMap API username, password
+#returns access token string
+
+def one_map_authorise(username, password):
+  auth_url = "https://www.onemap.gov.sg/api/auth/post/getToken"
+
+  payload = {
+          "email": username,
+          "password": password
+          }
+
+  response = requests.request("POST", auth_url, json=payload)
+  json_data = json.loads(response.text)
+
+  return json_data['access_token']
+
+
+# this function takes in two values, x and y coordinates.
+# it returns two strings: the latitude and longitude corresponding to these x and y coordinates
+def coordinates_to_lat_long(x, y, ONEMAP_TOKEN):
+    location = x + ',' + y
+    url = 'https://www.onemap.gov.sg/api/public/revgeocodexy'
+    headers = {"Authorization": ONEMAP_TOKEN}
+    params = {
+        'location': location,
+        'buffer': 40,
+        'addressType': 'All',
+        'otherFeatures': 'N'
+    }
+    
+    try:
+      response = requests.get(url, params=params, headers=headers)
+      response = response.json()['GeocodeInfo']
+
+      lat, long = response[0]['LATITUDE'], response[0]['LONGITUDE']
+    except:
+      return ("NA", "NA")
+    else:
+      return lat, long
+    
+
+#this function takes in two values: lat and long
+##returns two strings: lat and long values of the property respectively
+def get_planning_area_name_from_lat_long(lat, long, ONEMAP_TOKEN):
+  if (lat=="NA" or long=="NA"):
+    return "NA"
+
+  planning_query_url = "https://www.onemap.gov.sg/api/public/popapi/getPlanningarea"
+  headers = {"Authorization": ONEMAP_TOKEN}
+  params = {
+          "latitude": lat,
+          "longitude": long
+          }
+
+  response = requests.request("GET", planning_query_url, params = params, headers=headers)
+  try:
+    json_data = json.loads(response.text)
+  except:
+    return "NA"
+  else:
+    try: 
+      json_data[0]['pln_area_n']
+    except KeyError:
+      return "NA"
+    else:
+      return json_data[0]['pln_area_n']
+    
+# this function takes in a URA dataset, and assigns the lat and long based on the helper function above.
+def assign_long_lat_to_dataset(dataset, ONEMAP_TOKEN):
+  for property in dataset:
+    if 'x' in property and 'y' in property:
+      property['lat'], property['long'] = coordinates_to_lat_long(property['x'], property['y'], ONEMAP_TOKEN)
+
+
+#this function takes in a search string (assumes previously concatenated by user),
+##returns two strings: lat and long values of the property respectively
+def addr_to_lat_long (search_term):
+  data = {
+    "searchVal": search_term,
+    "returnGeom": "Y",
+    "getAddrDetails": "Y"
+    }
+  search_url = "https://www.onemap.gov.sg/api/common/elastic/search"
+
+  response = requests.request("GET", search_url, params=data)
+  #attempt to process API response and assign lat and long values
+  try:
+    json_data = json.loads(response.text)
+    lat = json_data["results"][0]["LATITUDE"]
+    long = json_data["results"][0]["LONGITUDE"]
+  #if json processing fails (for reasons such as empty json response due to invalid address) return "NA" for lat and long
+  except:
+    return ("NA", "NA")
+  else:
+    return lat, long
---
+++ etl_pipeline.py
@@ -0,0 +1,58 @@
+from airflow.decorators import dag, task
+from datetime import datetime, timedelta
+#import json to process API responses
+import json
+#import requests to handle API calls
+import requests
+#import pandas for data wrangling
+import pandas as pd
+
+from etl_helper import one_map_authorise, assign_long_lat_to_dataset, get_planning_area_name_from_lat_long
+
+################################### KEYS #######################################
+# fill in following. Running API calls to get access tokens through VS Code / Collab always results in errors, use Postman
+
+# GET https://www.ura.gov.sg/uraDataService/insertNewToken.action -H "AccessKey: accesskey"
+ONEMAP_USERNAME = ""
+ONEMAP_PASSWORD = ""
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 1, 1),
+    "email": [""],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 3,
+    "retry_delay": timedelta(minutes=10)
+}
+
+@dag(dag_id='is3107_project_etl', default_args=default_args, schedule=None, catchup=False, tags=['final_project'])
+def property_prices_etl():
+    onemap_token = ''
+    @task
+    def authorise():
+        onemap_token = one_map_authorise(ONEMAP_USERNAME, ONEMAP_PASSWORD)
+        return onemap_token
+    @task
+    def extract():
+        private_property_dataset_paths = ['privatepropertypricesbatch1(1).json', 'privatepropertypricesbatch2(1).json', 'privatepropertypricesbatch3(1).json', 'privatepropertypricesbatch4(1).json']
+        hdb_resale_dataset_path = 'ResaleflatpricesbasedonregistrationdatefromJan2017onwards.csv'
+        return private_property_dataset_paths, hdb_resale_dataset_path
+    def transform(private_property_dataset_paths, hdb_resale_dataset_path):
+        private_property_dataset_edited_paths = ['privatepropertypricesbatch1edited.json', 'privatepropertypricesbatch2edited.json'
+                                                 , 'privatepropertypricesbatch3edited.json', 'privatepropertypricesbatch4edited.json']
+        private_property_datasets = []
+        # open private property files, convert them into dictionaries from JSON
+        for path in private_property_dataset_paths:
+            with open(path, 'r') as f:
+                private_property_datasets.append(json.load(f)['Result'])
+        # add long, lat and planning area into dictionaries for private properties
+        for count, dataset in enumerate(private_property_datasets):
+            dataset = assign_long_lat_to_dataset(dataset, onemap_token)
+            print(dataset)
+            dataset['planning_area'] = dataset.apply(lambda x: get_planning_area_name_from_lat_long(x['lat'], x['long'], onemap_token))
+            with open(private_property_dataset_edited_paths[count], 'w') as file:
+                  file.write(dataset)
+    onemap_token =  authorise()   
+    private_property_dataset_paths, hdb_resale_dataset_path = extract()
+    transform(private_property_dataset_paths, hdb_resale_dataset_path, onemap_token)
