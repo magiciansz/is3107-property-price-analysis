2a66f83d80c91e3bc2e4291bfae29bd8a544c9e6
magiciansz <80561550+magiciansz@users.noreply.github.com>
2024-03-24 00:30:34 +0800

    upload updated sql

--- src/create_tables_clean.sql
+++ src/create_tables_clean.sql
@@ -1,66 +1,66 @@
 
 CREATE TABLE IF NOT EXISTS `District` (
-    `district_id` int NOT NULL ,
+    `id` int AUTO_INCREMENT NOT NULL,
     `district_name` varchar(50) NOT NULL ,
-    `coordinates` varchar(255) ,
+    `coordinates` LONGTEXT,
     PRIMARY KEY (
-        `district_id`
-    )
+        `id`
+    ),
+    UNIQUE KEY (district_name)
 );
 
 CREATE TABLE IF NOT EXISTS `Project` (
-    `project_id` int AUTO_INCREMENT NOT NULL ,
+    `id` int AUTO_INCREMENT NOT NULL ,
     `district_id` int  NOT NULL ,
-    `project_name` varchar(100) NOT NULL ,
-    `long` float,
-    `lat` float,
+    `project_name` varchar(100) NOT NULL,
+    `address` varchar(100) NOT NULL,
+    `long` DOUBLE,
+    `lat` DOUBLE,
     PRIMARY KEY (
-        `project_id`
+        `id`
     ),
-    UNIQUE KEY (project_name),
-    CONSTRAINT `fk_Project_district_id` FOREIGN KEY(`district_id`) REFERENCES `District` (`district_id`)
+    UNIQUE KEY (`address`, project_name, `long`, lat),
+    CONSTRAINT `fk_Project_district_id` FOREIGN KEY(`district_id`) REFERENCES `District` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Property` (
-    `property_id` int AUTO_INCREMENT NOT NULL ,
+    `id` int AUTO_INCREMENT NOT NULL ,
     `project_id` int  NOT NULL ,
     `property_type` varchar(25),
-    `street` varchar(100),
     `lease_year` smallint,
-    `lease_duration` smallint,
+    `lease_duration` int,
     `floor_range_start` tinyint,
     `floor_range_end` tinyint,
     `floor_area` float,
     PRIMARY KEY (
-        `property_id`
+        `id`
     ),
-    UNIQUE KEY (project_id, property_type, street, lease_year, lease_duration, floor_range_start, floor_range_end, floor_area),
-    CONSTRAINT `fk_Property_project_id` FOREIGN KEY(`project_id`) REFERENCES `Project` (`project_id`)
+    CONSTRAINT `fk_Property_project_id` FOREIGN KEY(`project_id`) REFERENCES `Project` (`id`)
 );
 
-CREATE TABLE IF NOT EXISTS `Transactions` (
-    `transaction_id` int AUTO_INCREMENT NOT NULL ,
+CREATE TABLE IF NOT EXISTS `Transaction` (
+    `id` int AUTO_INCREMENT NOT NULL ,
     `property_id` int  NOT NULL ,
     `transaction_year` smallint  NOT NULL ,
     `transaction_month` tinyint  NOT NULL ,
     `type_of_sale` varchar(15)  NOT NULL ,
     `price` float  NOT NULL ,
     PRIMARY KEY (
-        `transaction_id`
+        `id`
     ),
-	CONSTRAINT `fk_Transaction_property_id`  FOREIGN KEY(`property_id`) REFERENCES `Property` (`property_id`)
+	CONSTRAINT `fk_Transaction_property_id`  FOREIGN KEY(`property_id`) REFERENCES `Property` (`id`)
 );
 
 CREATE TABLE IF NOT EXISTS `Amenities` (
-    `amenity_id` int AUTO_INCREMENT NOT NULL ,
+    `id` int AUTO_INCREMENT NOT NULL ,
     `district_id` int  NOT NULL ,
     `amenity_type` tinyint  NOT NULL ,
     `long` float,
     `lat` float,
     PRIMARY KEY (
-        `amenity_id`
+        `id`
     ),
-    CONSTRAINT `fk_Amenities_district_id` FOREIGN KEY(`district_id`) REFERENCES `District` (`district_id`)
+    CONSTRAINT `fk_Amenities_district_id` FOREIGN KEY(`district_id`) REFERENCES `District` (`id`)
 );
 
 
