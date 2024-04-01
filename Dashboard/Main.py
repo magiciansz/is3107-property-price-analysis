78ee73e1631b6515ffff5b49c94aaa99b95ba93b
Chu Jia-Cheng <dorazhu@outlook.com>
2024-04-01 18:47:25 +0800

    add filter attribute in session states

--- Dashboard/Main.py
+++ Dashboard/Main.py
@@ -25,14 +25,37 @@ show_pages(
 ############### Add different parts to the session state ###################
 ############################################################################
 
-#amentity_list
-#district_list
-#price_range
+#TODO: retreive from DB
+#retreive [property, lat, long, name] from pd.dataframe
+# fileter later
 
+if "room_type" not in st.session_state: # HDB-2 ROOM - HDB 3ROOM
+    st.session_state['room_type'] = None #TODO: filter from DF
+
+if "transaction_year" not in st.session_state: # Year-Month datetime, 2 drop down list
+    st.session_state['transaction_year'] = None #TODO: filter from DF
+    
+if "transaction_month" not in st.session_state: 
+    st.session_state['transaction_month'] = None #TODO: filter from DF
+
+if "district_list" not in st.session_state: 
+    st.session_state['district_list'] = None #TODO: filter from DF
+    
 if "amentity_list" not in st.session_state:
     st.session_state['amentity_list'] = None #TODO: retreive from DB
-if "district" not in st.session_state:
-    st.session_state['district'] = None #TODO: retreive from DB
+
+if "price_per_sqft_min" not in st.session_state:
+    st.session_state['price_per_sqft_min'] = None #TODO: retreive from DB
+
+if "price_per_sqft_max" not in st.session_state:
+    st.session_state['price_per_sqft_max'] = None #TODO: retreive from DB
+
+if "floor_range_min" not in st.session_state:
+    st.session_state['floor_range_min'] = None #TODO: retreive from DB
+
+if "floor_range_max" not in st.session_state:
+    st.session_state['floor_range_max'] = None #TODO: retreive from DB
+
     
 ############################################################################
 st.title("Singapore Property Trend Chart")
