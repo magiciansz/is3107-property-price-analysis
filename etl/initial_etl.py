57efb87df48556c7dda74393de29655f6e56634e
magiciansz <80561550+magiciansz@users.noreply.github.com>
2024-04-01 12:34:20 +0800

    complete initial etl for local mysql

--- etl/initial_etl.py
+++ etl/initial_etl.py
@@ -22,11 +22,11 @@ from DataParser import DataParser
 from UpdateDB import UpdateDB
 from RetrieveDB import RetrieveDB
 
-dbupdate = UpdateDB(db_connect_type="IAM")
-dbretrieve = RetrieveDB()
+dbupdate = UpdateDB("LOCAL")
+dbretrieve = RetrieveDB("LOCAL")
 etl_helper = EtlHelper()
-kml = DataParser()
 
+kml = DataParser()
 import os
 from dotenv import load_dotenv, find_dotenv
 _ = load_dotenv(find_dotenv())
@@ -66,11 +66,6 @@ default_args = {
 
 @dag(dag_id='is3107_project_etl', default_args=default_args, schedule=None, catchup=False, tags=['final_project'])
 def property_prices_initial_etl():
-    # @task
-    # def authorise():
-    #     onemap_access_token = etl_helper.one_map_authorise(ONEMAP_USERNAME, ONEMAP_PASSWORD)
-    #     ura_access_token = etl_helper.ura_authorise(URA_ACCESS_KEY)
-    #     return [onemap_access_token, ura_access_token]
 
     @task
     def authorise_onemap():
@@ -83,7 +78,7 @@ def property_prices_initial_etl():
          return ura_access_token
     
     @task
-    def create_tables_db(create_tables_sql):
+    def create_tables_db(upstream_task_flag, create_tables_sql):
         """create tables in db
 
         Args:
@@ -96,14 +91,9 @@ def property_prices_initial_etl():
     def extract_planning_area(onemap_access_token):
         return etl_helper.extract_planning_area_polygon(onemap_access_token, "2024")
     
-    def extract_districts(onemap_access_token):
-        districts_dataset_path = DATA_FOLDER + '/' + DISTRICTS_EXTRACT_PATH + '.json'
-        with open(districts_dataset_path, 'w') as f:
-            json.dump({'results': etl_helper.extract_planning_area_polygon()}, f)
-        return districts_dataset_path
            
     @task
-    def extract_ura(ura_access_token):
+    def extract_ura(upstream_task_flag, ura_access_token):
         # TODO maybe use API here & define filenames outside
         ura_prices_data = {'Result': []}
         for batch in URA_BATCHES:
@@ -118,7 +108,7 @@ def property_prices_initial_etl():
         return ura_prices_dataset_path
     
     @task
-    def extract_hdb():
+    def extract_hdb(upstream_task_flag):
         today = time.strftime("%Y-%m")
         list_of_year_months_to_date = etl_helper.get_list_of_year_months(START_YEAR_MONTH_HDB, today)
         #initilize dict to store results
@@ -134,28 +124,23 @@ def property_prices_initial_etl():
         return hdb_prices_dataset_path
 
     @task
-    def extract_amenity():
-        amenity_url_dict = kml.download_amenity_files(output_folder = DATA_FOLDER, first_time = True)
-        print("Task 1 Complete!\n")
-        return amenity_url_dict
+    def extract_amenities(upstream_task_flag):
+        amenities_dataset_path_dict = kml.download_amenity_files(output_folder = DATA_FOLDER, first_time = True)
+        return amenities_dataset_path_dict
         
     @task
-    def transform_districts(districts_dataset_path):
-        x = pd.read_json(districts_dataset_path)
-        x['pln_area_n'] = x['results'].apply(lambda x: x['pln_area_n'])
-        x['coord_list'] = x['results'].apply(lambda x: eval(x['geojson'])['coordinates'][0][0])
-        x = x.drop("results", axis=1)
-        x_dict = x.to_dict()
-        districts_dataset_final_path = DATA_FOLDER + '/' + DISTRICTS_ADDED_FIELDS_PATH + '.json'
-        with open(districts_dataset_final_path, 'w') as file:
-            file.write(json.dumps({'Result': x_dict}))
+    def extract_districts(upstream_task_flag, onemap_access_token):
+        districts_dataset_path = "{DATA_FOLDER}/districts_initial.json".format(DATA_FOLDER = DATA_FOLDER)
+        with open(districts_dataset_path, 'w') as f:
+            json.dump({'results': etl_helper.extract_planning_area_polygon(onemap_access_token)}, f)
+        return districts_dataset_path
          
     @task
-    def transform_ura(ura_prices_dataset_path, onemap_access_token):
+    def transform_ura(upstream_task_flag, ura_prices_dataset_path, onemap_access_token):
         # open private property files, convert them into dictionaries from JSON
         ura_prices_dataset_final_path = "{DATA_FOLDER}/ura_prices_added.json".format(DATA_FOLDER = DATA_FOLDER)
         with open(ura_prices_dataset_path, 'r') as f:
-            dataset = json.load(f)['Result']
+            dataset = json.load(f)['Result'][:5]
             dataset = etl_helper.assign_long_lat_to_ura_dataset(dataset, onemap_access_token)
             dataset = etl_helper.assign_planning_area_to_ura_dataset(dataset, onemap_access_token)
             
@@ -170,15 +155,15 @@ def property_prices_initial_etl():
         return ura_combined_df_path
     
     @task
-    def transform_hdb(hdb_prices_dataset_path, onemap_access_token):
+    def transform_hdb(upstream_task_flag, hdb_prices_dataset_path, onemap_access_token):
         hdb_prices_dataset_final_path = "{DATA_FOLDER}/hdb_prices_added.json".format(DATA_FOLDER = DATA_FOLDER)
         with open(hdb_prices_dataset_path, 'r') as f:
-            dataset = json.load(f)['Result']
+            dataset = json.load(f)['Result'][:5]
             dataset = etl_helper.assign_long_lat_to_hdb_dataset(dataset)
             dataset = etl_helper.assign_planning_area_to_hdb_dataset(dataset, onemap_access_token)
             
             with open(hdb_prices_dataset_final_path, 'w') as file:
-                file.write(json.dumps(dataset))
+                file.write(json.dumps({'Result': dataset}))
         # massage hdb resale dataset
         hdb_combined_df = kml.parse_hdb(hdb_prices_dataset_final_path)  
         hdb_combined_df_path = "{DATA_FOLDER}/hdb_combined_df.csv".format(DATA_FOLDER = DATA_FOLDER)
@@ -186,57 +171,73 @@ def property_prices_initial_etl():
         return hdb_combined_df_path
 
     @task
-    def transform_amenity(amenity_url_dict, onemap_access_token):        
+    def transform_amenities(upstream_task_flag, amenities_dataset_path_dict, onemap_access_token):
         amenity_src_folder_path = DATA_FOLDER
         amenity_out_folder_path = amenity_src_folder_path
 
-        combined_amenities_file_path = kml.amenity_data_transformation_pipeline(amenity_url_dict, amenity_src_folder_path, amenity_out_folder_path, onemap_access_token)
-        print(combined_amenities_file_path)
+        combined_amenities_file_path = kml.amenity_data_transformation_pipeline(amenities_dataset_path_dict, amenity_src_folder_path, amenity_out_folder_path, onemap_access_token)
         return combined_amenities_file_path
+    
+    @task
+    def transform_districts(upstream_task_flag, districts_dataset_path):
+        x = pd.read_json(districts_dataset_path)
+        x['pln_area_n'] = x['results'].apply(lambda x: x['pln_area_n'])
+        x['coord_list'] = x['results'].apply(lambda x: eval(x['geojson'])['coordinates'][0][0])
+        x = x.drop("results", axis=1)
+        x_dict = x.to_dict()
+        districts_dataset_final_path = "{DATA_FOLDER}/districts_initial_added.json".format(DATA_FOLDER = DATA_FOLDER)
+        with open(districts_dataset_final_path, 'w') as file:
+            file.write(json.dumps({'Result': x_dict}))
+        return districts_dataset_final_path
          
     @task
-    def load_districts(district_path):
+    def load_districts(upstream_task_flag, district_path):
         district_df = etl_helper.load_district_df(district_path)
         dbupdate.load_district_table(district_df)
         return
     @task
-    def load_projects(hdb_combined_df_path, ura_combined_df_path):
+    def load_projects(upstream_task_flag, hdb_combined_df_path, ura_combined_df_path):
         project_df = etl_helper.load_hdb_ura_to_project(hdb_combined_df_path, ura_combined_df_path)
         dbupdate.load_project_table(project_df)
         return
     @task
-    def load_properties(hdb_combined_df_path, ura_combined_df_path):
+    def load_properties(upstream_task_flag, hdb_combined_df_path, ura_combined_df_path):
         property_df = etl_helper.load_hdb_ura_to_property(hdb_combined_df_path, ura_combined_df_path)
         dbupdate.load_property_table(property_df)
         return
     @task
-    def load_transactions(hdb_combined_df_path, ura_combined_df_path):
+    def load_transactions(upstream_task_flag, hdb_combined_df_path, ura_combined_df_path):
         transaction_df = etl_helper.load_hdb_ura_to_transaction(hdb_combined_df_path, ura_combined_df_path)
         dbupdate.load_transaction_table(transaction_df)
         return transaction_df
     
     @task
-    def load_amenities(amenities_combined_df_path):
+    def load_amenities(upstream_task_flag, amenities_combined_df_path):
         amenities_df = etl_helper.load_amenities_df(amenities_combined_df_path)
         dbupdate.load_amenity_table(amenities_df)
         return
 
-    # Execution Pipeline
-    onemap_access_token, ura_access_token = authorise_onemap(), authorise_ura()
+    # from scratch
+    onemap_access_token = authorise_onemap()
+    ura_access_token =  authorise_ura()
+    hdb_prices_dataset_path = extract_hdb(onemap_access_token)
+    ura_prices_dataset_path = extract_ura(hdb_prices_dataset_path, ura_access_token)
+    amenities_dataset_path_dict = extract_amenities(ura_prices_dataset_path)
+    districts_dataset_path = extract_districts(amenities_dataset_path_dict, onemap_access_token)
     
-    hdb_prices_dataset_path, ura_prices_dataset_path = extract_hdb(), extract_ura(ura_access_token)
-    hdb_combined_df_path, ura_combined_df_path = transform_hdb(hdb_prices_dataset_path, onemap_access_token), transform_ura(ura_prices_dataset_path, onemap_access_token)
-    amenity_url_dict = extract_amenity()
-    combined_amenities_df_path = transform_amenity(amenity_url_dict, onemap_access_token)
-
+    hdb_combined_df_path = transform_hdb(districts_dataset_path, hdb_prices_dataset_path, onemap_access_token)
+    ura_combined_df_path = transform_ura(hdb_combined_df_path, ura_prices_dataset_path, onemap_access_token)
+    amenities_combined_df_path = transform_amenities(ura_combined_df_path, amenities_dataset_path_dict, onemap_access_token)
+    districts_final_dataset_path = transform_districts(amenities_combined_df_path, districts_dataset_path)
+    
     # create tables + load
-    create_tables_db(CREATE_TABLES_SQL_PATH)
-    
-    load_districts('../Data/districts_final.json')
-    load_projects(hdb_combined_df_path, ura_combined_df_path)
-    load_properties(hdb_combined_df_path, ura_combined_df_path)
-    load_transactions(hdb_combined_df_path, ura_combined_df_path)
-    load_amenities(combined_amenities_df_path)
+    tables_created_flag = create_tables_db(districts_final_dataset_path, CREATE_TABLES_SQL_PATH)
+
+    districts_loaded_flag = load_districts(tables_created_flag, districts_final_dataset_path)
+    projects_loaded_flag = load_projects(districts_loaded_flag, hdb_combined_df_path, ura_combined_df_path)
+    properties_loaded_flag = load_properties(projects_loaded_flag, hdb_combined_df_path, ura_combined_df_path)
+    transactions_loaded_flag = load_transactions(properties_loaded_flag, hdb_combined_df_path, ura_combined_df_path)
+    load_amenities(transactions_loaded_flag, amenities_combined_df_path)
 
 # end define DAG
 
