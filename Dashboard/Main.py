256b176dc1fefd16815e17556464304536df0a33
Chu Jia-Cheng <dorazhu@outlook.com>
2024-04-05 17:05:32 +0800

    create init for session states

--- Dashboard/Main.py
+++ Dashboard/Main.py
@@ -8,6 +8,7 @@ import visualizations as v
 import sys
 from pathlib import Path
 from Filter import Filter
+from create_init import init_session_state
 
 current_script_path = Path(__file__).resolve()
 parent_directory = current_script_path.parent.parent
@@ -44,56 +45,11 @@ show_pages(
 ############################################################################
 ############### Add different parts to the session state ###################
 ############################################################################
-if "filter" not in st.session_state:
-    filter = Filter()
-    st.session_state['filter'] = filter
-
-if "all_transactions" not in st.session_state:
-    st.session_state['all_transactions'] = pd.DataFrame(st.session_state.cursor.get_price_per_sqft_dashboard())
-
-if "room_type" not in st.session_state:
-    st.session_state['room_type']  = sorted(st.session_state.all_transactions['property_type'].unique())
-    st.session_state.filter.room_type = st.session_state['room_type']
-    # TODO consider using this or type_of_sale from transaction table
-
-# if "transaction_year" not in st.session_state:
-#     st.session_state['transaction_year'] = st.session_state.all_transactions['transaction_year'].unique()
-#     filter.transaction_year = st.session_state['transaction_year']
-    
-# if "transaction_month" not in st.session_state: 
-#     st.session_state['transaction_month'] = st.session_state.all_transactions['transaction_month'].unique()
-#     filter.transaction_month = st.session_state['transaction_month']
-
-if "transaction_date_start" not in st.session_state:
-    st.session_state['transaction_date_start'] = datetime.date(2019,1,1)
-    
-if "transaction_date_end" not in st.session_state: 
-    st.session_state['transaction_date_end'] = datetime.datetime.now()
-
-
-if "district_list" not in st.session_state:
-    st.session_state.district_list = sorted(st.session_state.all_transactions['district_name'].unique())
-    st.session_state.filter.district_list = st.session_state.district_list
-
-if "amenities_list" not in st.session_state:
-    amenities = pd.DataFrame(st.session_state.cursor.get_amenities())
-    st.session_state.amenities = amenities
-    st.session_state['amenities_list'] = ['MRT','Park']
-    st.session_state.filter.amenities_list = set(amenities['amenity_type'])
-    
-if "price_per_sqft_range" not in st.session_state:
-    st.session_state['price_per_sqft_range'] = (st.session_state.all_transactions['price_per_sqft'].tolist()[0], st.session_state.all_transactions['price_per_sqft'].tolist()[-1])
-    st.session_state.filter.price_per_sqft_range = st.session_state['price_per_sqft_range']
-    
-# if "floor_range" not in st.session_state:
-#     st.session_state['floor_range'] = (st.session_state.all_transactions['floor_range_start'].min(),st.session_state.all_transactions['floor_range_start'].max())
-#     st.session_state.filter.floor_range = st.session_state['floor_range']
-
 
 
 
 ############################################################################
-
+init_session_state()
 st.title("Singapore Property Price Trend")
 
 on = st.toggle('Show Filters')
@@ -160,7 +116,7 @@ except: #avoid error during user selection
 # values to be put into filter
 st.write("TESTING")
 st.write(st.session_state['all_transactions'])
-# st.write(st.session_state.district_list)
+st.write(st.session_state.district_list)
 # st.write(st.session_state.amenities_list)
 # st.write(st.session_state.floor_range_max)
 # st.write(st.session_state.floor_range_min)
--- Dashboard/visualizations.py
+++ Dashboard/visualizations.py
@@ -5,57 +5,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from branca.colormap import linear
 
-def get_geojson_district(districts):
-    features = []
-    for idx, row in districts.iterrows():
-        feature = {
-            "type": "Feature",
-            "properties": {
-                "name": row['district_name'],
-                'number of projects': row['no_of_projects'],
-                'average price': row['avg_dist_price_per_sqft']
-            },
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [ast.literal_eval(row['coordinates'])]
-            }
-        }
-        features.append(feature)
 
-    geojson_district = {
-        "type": "FeatureCollection",
-        "features": features
-    }
-
-    return geojson_district
-
-def plot_price_per_district(data):
-    #TODO colormap with average price 
-    districts = get_geojson_district(data)
-    
-    # colormap = linear.YlGn_09.scale(
-    #     320000, 900000000
-    # )
-
-
-    m = folium.Map(location=[1.3521,103.8198], #center of singapore
-               zoom_start = 11) #initialize the map in singapore
-
-    folium.GeoJson(
-        districts,
-        highlight_function=lambda feature: {
-            "fillColor": 'green',
-        },
-        style_function= lambda feature: {
-            'fillColor' :'grey',
-            "color": "black",
-            "weight": 1,
-        },
-        popup= folium.GeoJsonPopup(fields=["name",'number of projects', 'average price']),
-        popup_keep_highlighted=True,
-    ).add_to(m)
-
-    return m
 
 def plot_price_over_time(data, start_date, end_date, room_type_list, district_list, price_per_sqft_range):
     #TODO: can provide district_list to add price line
