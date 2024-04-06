02a58cdb4d905303c9f92521e2bfb4be7f7f6701
zhangyu <xzy.zhangyu@gmail.com>
2024-04-06 11:47:46 +0800

    expander widget filter

--- Dashboard/create_init.py
+++ Dashboard/create_init.py
@@ -12,8 +12,9 @@ def init_session_state():
         st.session_state['all_transactions'] = pd.DataFrame(st.session_state.cursor.get_price_per_sqft_dashboard())
 
     if "room_type" not in st.session_state:
-        st.session_state['room_type']  = sorted(st.session_state.all_transactions['property_type'].unique())
-        st.session_state.filter.room_type = st.session_state['room_type']
+        # st.session_state['room_type']  = sorted(st.session_state.all_transactions['property_type'].unique())
+        st.session_state.filter.room_type = sorted(st.session_state.all_transactions['property_type'].unique())
+        st.session_state['room_type'] = []
         # TODO consider using this or type_of_sale from transaction table
 
     if "transaction_date_start" not in st.session_state:
@@ -25,8 +26,8 @@ def init_session_state():
     if "district_list" not in st.session_state:
         district_project_record = pd.DataFrame(st.session_state.cursor.get_district_popup())
         district_project_record = sorted(district_project_record[district_project_record['no_of_projects'] > 0]['district_name'])
-        st.session_state.district_list=district_project_record
-        st.session_state.filter.district_list = st.session_state.district_list
+        st.session_state.district_list=[]
+        st.session_state.filter.district_list = district_project_record
 
     if "amenities_list" not in st.session_state:
         amenities = pd.DataFrame(st.session_state.cursor.get_amenities())
